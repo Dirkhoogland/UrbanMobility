989dff461e2e596eab8ddcd76cfd3caa210c5f9f
Dirkhoogland <99063413@mydavinci.nl>
2025-05-29 15:35:24 +0200

    start menu

---
+++ Menus.py
@@ -0,0 +1,29 @@
+﻿def toon_dynamisch_menu(opties, titel="Hoofdmenu"):
+    langste_optie = max([len(f"{i+1}. {optie}") for i, optie in enumerate(opties)])
+    breedte = max(len(titel), langste_optie) + 4  # 4 extra voor ruimte
+
+    print("╔" + "═" * breedte + "╗")
+    print("║" + f" {titel} ".center(breedte) + "║")
+    print("╠" + "═" * breedte + "╣")
+
+    for i, optie in enumerate(opties, start=1):
+        regel = f"{i}. {optie}"
+        print("║ " + regel.ljust(breedte - 1) + "║")
+
+    print("╚" + "═" * breedte + "╝")
+
+def service():
+    return ["1. Update Scooter","2. Get Scooter attributes", "3. Update Password", "4. Logout"]
+
+def system():
+    return ["Add/modify Service Engineer", "Check Users", "Update Scooter","Get Scooter attributes", "Update Password", "Update Profile","Delete account", "Logout"]
+
+def super():
+    return ["Add/modify Service Engineer", "Check Users", "Update Scooter","Get Scooter attributes", "Update Password", "Logout"]
+
+def addmodifyengineermenu():
+    return ["Add new Service Engineer", "Modifty Service Engineer", "Remove Service Engineer", "Reset Service Engineer Password",]
+
+
+
+
--- UrbanMobility.py
+++ UrbanMobility.py
@@ -1,9 +1,20 @@
-import DatabaseSetup
+﻿import DatabaseSetup
 import Databasefunctions
-
-DatabaseSetup.Databasesetupstart()
+import Menus
+# programming debug for editing db
+# DatabaseSetup.Databasesetupstart()
 
 startup = True;
+
+def getuserrank(rank):
+    if rank == 0:
+        return "Super Administrator"
+    if rank == 1:
+        return "System Administrator"
+    if rank == 2:
+        return "Service Engineer"
+
+
 
 def start():
     login = True;
@@ -15,9 +26,26 @@ def start():
         check = Databasefunctions.login(username, password)
         if check == True: 
             login = False
-
+    # gets rank number and name
     user = Databasefunctions.getuserdetails(username)
-    print(f"Welkom bij het UrbanMobility project {user[1]} rank {user[0]}")
+    # plaatst het in een naam die gebruikelijk is voor de user
+    ranking = getuserrank(user[0]);
+    print(f"Welkom bij het UrbanMobility project {user[1]} rank {ranking}")
+
+    mainmenu = True
+    while mainmenu == True:
+        print(f"Hallo {ranking} {user[1]} ")
+
+        opties = " "
+        if user[0] == 0:
+            opties = Menus.super()
+        if user == 1:
+            opties = Menus.system()
+        if user[0] == 2:
+            opties = Menus.service()
+        menu = Menus.toon_dynamisch_menu(opties)
+        optie = input("Wat wil je openen")
+        
 
 
 
