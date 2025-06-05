73e392ae23cc5f65a570dca2db2a8bf809be87fd
BoraHR <1066706@hr.nl>
2025-06-05 15:36:17 +0200

    Fixed loop when logged in (by adding input)

--- UrbanMobility.py
+++ UrbanMobility.py
@@ -1,8 +1,9 @@
 ﻿import DatabaseSetup
 import Databasefunctions
 import Menus , Servicemedewerker
+from Menus import toon_dynamisch_menu
 # programming debug for editing db
-DatabaseSetup.Databasesetupstart()
+# DatabaseSetup.Databasesetupstart()
 
 startup = True;
 
@@ -15,9 +16,7 @@ def getuserrank(rank):
         return "Service Engineer"
 
 
-
-
-def start():
+def Start():
     login = True;
     while login == True:
         username = input("Vul je username in: ")
@@ -30,23 +29,73 @@ def start():
     # gets rank number and name
     user = Databasefunctions.getuserdetails(username)
     # plaatst het in een naam die gebruikelijk is voor de user
-    ranking = getuserrank(user[1]);
-    print(f"Welkom bij het UrbanMobility project {user[2]} rank {ranking}")
+    ranking = getuserrank(user[0]);
+    print(f"Welkom bij het UrbanMobility project {user[1]} rank {ranking}")
 
     mainmenu = True
     while mainmenu == True:
-        print(f"Hallo {ranking} {user[2]} ")
+        print(f"Hallo {ranking} {user[1]} ")
+        print()
 
-        opties = " "
-        if user[1] == 0:
-            opties = Menus.super()
-        if user[1] == 1:
-            opties = Menus.system()
-        if user[1] == 2:
-            Servicemedewerker.ServiceMenu(user)
+        if user[0] == 0:
+            toon_dynamisch_menu(Menus.super(), "Super Administrator Menu")
+            
+        if user == 1 or user[0] == 2:
+            toon_dynamisch_menu(Menus.service())
+            
 
+        print()
+        choice = input("select your option: ")[0]
+
+        # Admin:
+        # 1. Add/modify Service Engineer
+        # 2. Check Users
+        # 3. Update Scooter
+        # 4. Get Scooter attributes
+        # 5. Update Password
+        # 6. Logout
         
+        # Admin
+        # if user[0] == 0:
+        #     AdminOptions()
+
+        # if user == 1 or user[0] == 2:
 
 
+        # User?
+        # if user == 1:
+        #     if opties == 1:
 
-start();
+        #     if opties == 2:
+
+        #     if opties == 3:
+
+        # ServiceMedewerker
+        # if user[2] == 2:
+        #     if opties == 1:
+
+        #     if opties == 2:
+
+        #     if opties == 3:
+
+    # def AdminOptions(choice):
+        # if choice == 1:
+            
+        # if choice == 2:
+        
+        # if choice == 3:
+
+        # if choice == 4:
+
+        # if choice == 5:
+
+        # if choice == 6:
+
+    # def ServiceOptions(choice):
+        # if choice == 1:
+
+        # if choice == 2:
+
+        # if choice == 3:
+
+        # if choice == 4:
---
+++ program.py
@@ -0,0 +1,7 @@
+from DatabaseSetup import Databasesetupstart
+from UrbanMobility import Start
+
+if __name__ == "__main__":
+    print("versie 1.5") # om build te checken
+    Databasesetupstart()
+    Start()
