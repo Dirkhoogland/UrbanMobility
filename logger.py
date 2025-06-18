624bef90ac194ff6f4373cd56ff023bcc93f811b
BoraHR <1066706@hr.nl>
2025-06-18 13:04:21 +0200

    fixed issue with logs (cause dubble decryption)

--- logger.py
+++ logger.py
@@ -1,8 +1,11 @@
 import sqlite3
-from DatabaseSetup import db_path
+import os
 
 from Encrypt import Log_encrypt
 from Decrypt import Log_decrypt_many
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, "Database.db")
 
 def Encrypt_Log(Action, UserID, Username, Timestamp, Result, Serverity, Sus: bool):
     return Log_encrypt(Action, UserID, Username, Timestamp, Result, Serverity, Sus)
@@ -17,11 +20,12 @@ def Decrypte_all_logs():
 
         logs = cursor.execute('''
             SELECT * FROM ActionLog
-        )''').fetchall()
-
-    except sqlite3.OperationalError: 
-        print("Failed to retrieve logs")
+        ''').fetchall()
+        
+    except sqlite3.OperationalError as e: 
+        print(f"Failed to retrieve logs {e}")
     finally:
         if conn:
             conn.close()
-        return Log_decrypt_many(logs)
+        
+    return Log_decrypt_many(logs)
--- um_Dirk_1077619_Bora_1066706.py
+++ um_Dirk_1077619_Bora_1066706.py
@@ -3,15 +3,21 @@ from UrbanMobility import Start
 from Traveller import View, Update, AddTraveller, Delete, Add
 from Menus import toon_dynamisch_menu, cityOption
 from Encrypt import generate_key, get_key
+from Databasefunctions import *
 
 if __name__ == "__main__":
-    print("versie 1.27") # om build te checken
+    print("versie 1.31") # om build te checken
+    Databasesetupstart()
     # while True:
     #     if AccessPassword():
     #         CreateBackup()
     # print(View("anna.jansen@example.com"))
+    log_actie("TEST", "Super_admin", "Ok", "None", "Yes")
+    log_actie("TEST", "Super_admin", "Ok", "None", "No")
+    logs()
+    
     get_key()
-    Databasesetupstart()
+    
     # Update("anna.jansen@example.com")
     Delete("anna.jansen@example.com")
     Add("Anna", "Jansen", "1990-05-12", "F", "Lindelaan", 23, "1234AB", "Rotterdam", "anna.jansen@example.com", "+31-6-06123456", "NL1234567") # add Anna back for testing purposes
